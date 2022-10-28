55e236d3bcc665e0f6099d1cf3edcf16041395d3
ihsan <hasansaygin24@gmail.com>
2022-10-28 14:50:59 +0300

    about created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,12 @@ const About = () => {
     <div className="about">
       <div className="info">
         <h2>About software developer Ihsan</h2>
-        <p>My name is ihsan. I'm interested </p>
+        <p>
+          My name is ihsan. I'm from Erzincan. I'm 33 years old. I am interested
+          in react java and python In this project, data extraction was
+          performed using axios with the API key obtained from the Edemam.com
+          site.
+        </p>
       </div>
     </div>
   );
